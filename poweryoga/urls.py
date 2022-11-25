921466997d60a2140aa1f924d4eeddb1affb6797
MartinaP87 <bleach1987@hotmail.it>
2022-11-25 19:58:20 +0000

    display posts

--- poweryoga/urls.py
+++ poweryoga/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls'))
+    path('summernote/', include('django_summernote.urls')),
+    path('', include('yoga_blog.urls'), name='yoga_blog_urls')
 ]
