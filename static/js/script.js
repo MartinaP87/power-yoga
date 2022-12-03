aa9af630503f765ca0773d7476d683d4430687ba
MartinaP87 <bleach1987@hotmail.it>
2022-12-03 19:41:58 +0000

    Dispone classes in the table

--- static/js/script.js
+++ static/js/script.js
@@ -1,30 +1,74 @@
+function table_update() {
+    let days = document.getElementsByClassName("day_name")
+    for (let i = 0; i < days.length; i++) {
+        console.log(i)
+        let day = document.getElementsByClassName("day_name")[i];
+        let time = document.getElementsByClassName("time")[i];
+        let title = document.getElementsByClassName("class_title")[i];
+        let row_blocks = document.getElementsByClassName("rows");
+        let week_days_collection = document.getElementsByClassName("cols")[0].children
+        for (week_day in week_days_collection) {
+            if (day.innerText == week_days_collection[week_day].innerText) {
+                let new_class = week_days_collection[week_day].className;
+                title.classList.add(new_class);
+            }
+        }
+        for (let row_block = 0; row_block < row_blocks.length; row_block++) {
+            let time_titles = row_blocks[row_block].firstElementChild;
+            let time_rows = row_blocks[row_block].children;
+            if (time.innerText == time_titles.innerText) {
+                for (let time_row = 0; time_row < time_rows.length; time_row++) {
+                    let correct_class = time_rows[time_row].className;
+                    if (title.classList.contains(correct_class)) {
+                        time_rows[time_row].innerText = title.innerText;
+                        console.log(title.innerText)
+                    }
+                }
+            }
+        }}
 
-    // function js_test(){
-    //   console.log("function called");
-    //   let giorno1 = document.getElementsByClassName("day1");
-    //   console.log(giorno1);
-    //   console.log(giorno1[0].innerText);
-    //   for (var i = 0; i < giorno1.length; i++) {
-    //     let testo = giorno1[i]["innerText"];
-    //     console.log(testo)
-    //   }
-    // };
-    // js_test();
-    // let cacca = document.getElementsByClassName("day2").innerHtml
-    // for (i in cacca){
-    //     console.log(i)
-    // }
-    // console.log(cacca)
-    let collection = document.getElementsByClassName("day3");
-    console.log(collection)
-    let text = collection.innerHtml;
-    console.log(text)
+        
 
-    var x = document.getElementsByClassName("day1");
+    }
+
+table_update()
+
+// let days = document.getElementsByClassName("day_name")
+//     for (let i = 0; i < days.length; i++) {
+//         console.log(i)
+//         let day = document.getElementsByClassName("day_name")[i];
+//         let time = document.getElementsByClassName("time")[i];
+//         let title = document.getElementsByClassName("class_title")[i];
+//         let row_blocks = document.getElementsByClassName("rows");
+//         let week_days_collection = document.getElementsByClassName("cols")[0].children
+//         for (week_day in week_days_collection) {
+//             if (day.innerText == week_days_collection[week_day].innerText) {
+//                 let new_class = week_days_collection[week_day].className;
+//                 title.classList.add(new_class);
+//             }
+//         }
+// for (let row_block = 0; row_block < row_blocks.length; row_block++) {
+//     let time_titles = row_blocks[row_block].firstElementChild;
     
-    console.log(x)
-for (var i = 0; i < x.length; i++) {
-    console.log(x.item(i))
-    x[i].style.color = "blue";
-    console.log(x[i])
-}
+//     let time_rows = row_blocks[row_block].children;
+    
+//     if (time.innerText == time_titles.innerText) {
+//     console.log("yeah")
+//        for (let time_row = 0; time_row < time_rows.length; time_row++) {
+
+//            console.log("primo", time_rows)
+//            console.log("secondo", time_row)
+//            console.log("terzo", time_rows[time_row])
+           
+
+           
+//             let correct_class = time_rows[time_row].className;
+//                 console.log(correct_class)    
+//             if(title.classList.contains(correct_class)){
+//                 time_rows[time_row].innerText = title.innerText;
+//                 console.log(title.innerText)
+//                 console.log(time_rows[time_row].innerText)
+//                 }
+//             }
+//         }
+// }}
