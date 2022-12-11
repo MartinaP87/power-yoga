55f1ac09d20e03a4087e7b2c9ff694dfeb865734
MartinaP87 <bleach1987@hotmail.it>
2022-12-11 11:54:25 +0000

    limit reservation days

--- bookings/views.py
+++ bookings/views.py
@@ -27,37 +27,39 @@ def reservations(request):
 
 def book(request):
     week_days = get_days(request)
-    yoga_classes = YogaClass.objects.filter(status=1)
-
-    yoga_classes_available = yoga_classes.filter(
-        day__range=[week_days[0], week_days[6]])
-    yoga_classes_available_now = yoga_classes.filter(
-        day__range=[date.today(), week_days[6]])
-    print(yoga_classes_available_now)
-    print(yoga_classes_available)
-    
+    yoga_classes = weekly_classes(request)
+    yoga_classes_available_now = []
+    for yoga_class in yoga_classes:
+        if yoga_class.day >= date.today():
+            yoga_classes_available_now.append(yoga_class)
+    print("POSSIBILI CLASSI", yoga_classes_available_now)
+  
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
             new_reservation = form.save()
-            if request.user.id == new_reservation.member.id:
-                reserved_class_id = new_reservation.yoga_class_id
-                update_approval(request, new_reservation)
-                print(new_reservation.id)
-                print("PRIMO", new_reservation.member)
-                if new_reservation.approved:
-                    reduce_available_spaces(request, reserved_class_id)
-                    return redirect('reservations')
+            if new_reservation.yoga_class in yoga_classes_available_now:
+                if request.user.id == new_reservation.member.id:
+                    reserved_class_id = new_reservation.yoga_class_id
+                    update_approval(request, new_reservation)
+                    print(new_reservation.id)
+                    print("MEMBRO NEWRES", new_reservation.member)
+                    if new_reservation.approved:
+                        reduce_available_spaces(request, reserved_class_id)
+                        return redirect('reservations')
+                    else:
+                        print("fullybooked")
+                        reservation = get_object_or_404(
+                            Reservation, id=new_reservation.id)
+                        messages.error(
+                            request, 'Unfortunately the class is fully booked, choose another class!')
+                        new_reservation.delete()
                 else:
-                    print("fullybooked")
-                    reservation = get_object_or_404(
-                        Reservation, id=new_reservation.id)
-                    messages.error(
-                        request, 'Unfortunately the class is fully booked, choose another class!')
+                    messages.warning(
+                        request, "To book a class with a different account login with its details")
                     new_reservation.delete()
             else:
-                messages.warning(
-                    request, "To book a class with a different account login with its details")
+                messages.error(request, "This class is not longer available")
                 new_reservation.delete()
     form = ReservationForm()
     context = {
@@ -95,8 +97,7 @@ def reduce_available_spaces(request, chosen_class_id):
     updated_spaces = spaces - 1
     chosen_yoga_class.available_spaces = updated_spaces
     chosen_yoga_class.save()
-    print("bo")
-    print("YOGA", chosen_yoga_class.id, chosen_yoga_class.available_spaces)
+    print("CLASSE ID E SPAZIO-", chosen_yoga_class.id, chosen_yoga_class.available_spaces)
 
 
 def increase_available_spaces(request, chosen_class_id):
@@ -106,188 +107,23 @@ def increase_available_spaces(request, chosen_class_id):
     updated_spaces = spaces + 1
     chosen_yoga_class.available_spaces = updated_spaces
     chosen_yoga_class.save()
-    print("bodel")
-    print("YOGA", chosen_yoga_class.id, chosen_yoga_class.available_spaces)
+    print("CLASSE ID E SPAZIO+", chosen_yoga_class.id, chosen_yoga_class.available_spaces)
 
 
 def get_days(request):
     today = date.today()
     start = today - timedelta(days=today.weekday())
-    end = start + timedelta(days=6)
+    end = start + timedelta(days=20)
     dates = [start + timedelta(days=i) for i in range((end-start).days+1)]
     return (dates)
 
-# def available_classes(request):
-#     classes = get_object_or_404(YogaClass.objects.filter(status=1))
-#     for single_class in classes:
-#         if int(single_class.available_spaces) > 0:
 
-# def update_yoga_class(request):
-#     reservations = Reservation.objects.all()
-#     for reservation in reservations:
-#         yoga_class_id = reservation.yoga_class_id
-#         queryset = YogaClass.objects.filter(status=1)
-#         reserved_yoga_class = get_object_or_404(queryset, id=yoga_class_id)
-#         spaces = int(reserved_yoga_class.available_spaces)
-#         if reservation.approved:
-#             updated_spaces = spaces - 1
-#             reserved_yoga_class.available_spaces = updated_spaces
-#             reserved_yoga_class.save()
-#             print("bo")
-#         print("YOGA", reserved_yoga_class.id, reserved_yoga_class.available_spaces)
-# def book(request):
-    # queryset = YogaClass.objects.filter(status=1)
-    # yoga_class = get_object_or_404(queryset, id=yoga_class_id)
-    # if request.method == 'POST':
-    #     form = ReservationForm(request.POST)
-    #         yoga_class=yoga_class,
-    #         member=member
-    #     )
-    #     messages.success(request, "created appointment")
-    #     return redirect('home')
-    # return redirect('home')
-    # queryset = YogaClass.objects.filter(status=1)
-    # class_to_book = get_object_or_404(queryset, id=yoga_class_id)
-    # reservations = class_to_book.reservations.filter(approved=True)
-    # print(reservations)
-    # available_spaces = class_to_book.available_spaces
-    # reservation_form.fields["yoga_class"].queryset = YogaClass.objects.filter(
-    #     id=yoga_class_id)
-    # reservation_form = ReservationForm(data=request.POST)
-    # if reservation_form.is_valid():
-    #     print("form is valid")
-    #     reservation_form.instance.member = request.user
-    #     reservation = reservation_form.save(commit=False)
-    #     reservation.class_to_book = class_to_book
-    #     reservation.save()
-    #     messages.success(request, 'Your Reservation Was Successful!')
-    # availabele_spaces = int(availabele_spaces) - 1
-    # return redirect('my_bookings')
-    #     return redirect('home')
-    # else:
-    #     messages.info(
-    #         request, 'Unfortunately this class is fully booked.Why not picking another class?!')
-    #     return redirect('classes')
-    # else:
-    #     messages.info(request, 'Unfortunately...')
-    #     reservation_form = ReservationForm()
-    #     return render(
-    #         request,
-    #         "classes.html",
-    #     )
-
-
-# def add_reservation(request):
-#     if request.method == 'POST':
-#         form = ReservationForm(request.POST)
-#         if available_spaces > 0:
-#             if form.is_valid():
-#                 available_spaces = int(available_spaces) - 1
-#                 form.save()
-#                 return redirect('my_classes')
-#         else:
-#             messages.info(
-#                 request, 'Unfortunately this class is fully booked.\
-#                 Why not picking another class?!')
-#             return redirect('classes')
-#     form = ReservationForm()
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'classes.html', context)
-
-# class ClassDetail(View):
-
-#     def get(self, request, id, *args, **kwargs):
-#         queryset = YogaClass.objects.filter(status=1)
-#         yoga_section = get_object_or_404(queryset, id=id)
-#         reservations = yoga_section.reservations.filter(approved=True)
-#         return render(
-#             request,
-#             "classes.html",
-#             {
-#                 "yoga_section": yoga_section,
-#                 "reservations": reservations,
-#                 "reservation_form": ReservationForm()
-#             },
-#         )
-
-#     def post(self, request, slug, *args, **kwargs):
-
-#         queryset = Post.objects.filter(status=1)
-#         post = get_object_or_404(queryset, slug=slug)
-#         comments = post.comments.filter(approved=True).order_by(
-# "-created_on")
-#         liked = False
-#         if post.likes.filter(id=self.request.user.id).exists():
-#             liked = True
-
-#         comment_form = CommentForm(data=request.POST)
-#         if comment_form.is_valid():
-#             comment_form.instance.email = request.user.email
-#             comment_form.instance.name = request.user.username
-#             comment = comment_form.save(commit=False)
-#             comment.post = post
-#             comment.save()
-#         else:
-#             comment_form = CommentForm()
-
-#         return render(
-#             request,
-#             "post_detail.html",
-#             {
-#                 "post": post,
-#                 "comments": comments,
-#                 "commented": True,
-#                 "comment_form": comment_form,
-#                 "liked": liked
-#             },
-#         )
-#   class PostDetail(View):
-#     def get(self, request, slug, *args, **kwargs):
-#         queryset = Post.objects.filter(status=1)
-#         post = get_object_or_404(queryset, slug=slug)
-#         comments = post.comments.filter(approved=True).order_by(
-# "-created_on")
-#         liked = False
-#         if post.likes.filter(id=self.request.user.id).exists():
-#             liked = True
-#         return render(
-#             request,
-#             "post_detail.html",
-#             {
-#                 "post": post,
-#                 "comments": comments,
-#                 "commented": False,
-#                 "liked": liked,
-#                 "comment_form": CommentForm()
-#             },
-#         )
-#     def post(self, request, slug, *args, **kwargs):
-#         queryset = Post.objects.filter(status=1)
-#         post = get_object_or_404(queryset, slug=slug)
-#         comments = post.comments.filter(approved=True).order_by(
-# "-created_on")
-#         liked = False
-#         if post.likes.filter(id=self.request.user.id).exists():
-#             liked = True
-#         comment_form = CommentForm(data=request.POST)
-#         if comment_form.is_valid():
-#             comment_form.instance.email = request.user.email
-#             comment_form.instance.name = request.user.username
-#             comment = comment_form.save(commit=False)
-#             comment.post = post
-#             comment.save()
-#         else:
-#             comment_form = CommentForm()
-#         return render(
-#             request,
-#             "post_detail.html",
-#             {
-#                 "post": post,
-#                 "comments": comments,
-#                 "commented": True,
-#                 "comment_form": comment_form,
-#                 "liked": liked
-#             },
-#         )
+def weekly_classes(request):
+    week_days = get_days(request)
+    yoga_classes = YogaClass.objects.filter(status=1)
+    if yoga_classes:
+        for yoga_class in yoga_classes:
+            if yoga_class.day < week_days[0]:
+                yoga_class.delete()
+    print("DA INIZIO SETTIMANA", yoga_classes)
+    return yoga_classes
