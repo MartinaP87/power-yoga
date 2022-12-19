062b7ce678786624dcf99b80a0db984c5417d989
MartinaP87 <bleach1987@hotmail.it>
2022-12-19 21:45:14 +0000

    Add pagination yoga blog

--- yoga_blog/views.py
+++ yoga_blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
@@ -6,18 +7,23 @@ from .forms import CommentForm
 
 def post_list(request):
     """
-    Renders about page and passes the post list to the template.
+    It renders about page and passes the post list to the template.
+    It paginates the blog posts.
     """
     post_list = Post.objects.filter(status=1).order_by("-created_on")
+    paginator = Paginator(post_list, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'post_list': post_list
+        'post_list': post_list,
+        'page_obj': page_obj
     }
     return render(request, "yoga_blog/blog.html", context)
 
 
 def post_detail(request, slug):
     """
-    Renders post detail and passes posts, comments,
+    It renders post detail and passes posts, comments,
     comment form, and likes to the template.
     Handles comment form validation and sends a message
     to the user when the form is submitted successfully.
@@ -56,7 +62,7 @@ def post_detail(request, slug):
 
 def post_like(request, slug):
     """
-    Toggles likes; It removes the like if it already
+    It toggles likes; It removes the like if it already
     exists or adds the like if previously not applied.
     """
     post = get_object_or_404(Post, slug=slug)
